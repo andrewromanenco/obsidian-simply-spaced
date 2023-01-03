cfeb686d58dd4d539b13ce42a911e1b99ab9fcd1
Andrew Romanenco <andrew@romanenco.com>
2023-01-02 22:06:50 -0500

    Add UI
    Basic UI to present the question with control options.

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice,
+	Plugin, PluginSettingTab, Setting, MarkdownRenderer, TextComponent, Component,
+DataWriteOptions,
+TFile,
+Vault} from 'obsidian';
+
+import {CardsDB, Card} from 'cardsdb';
+import {getNextInterval, Answer} from 'spacer';
+import {LearningSession} from 'learning-session';
 
 // Remember to rename these classes and interfaces!
 
@@ -14,124 +22,139 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Simply spaced', (evt: MouseEvent) => {
+			new QnAModal(this.app, this).open();
 		});
-		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class QnAModal extends Modal {
+	private parentComponent: Component;
+	private vault: Vault;
+	private learningSession: LearningSession;
+
+	private root: HTMLDivElement;
+
+	constructor(app: App, parentComponent: Component) {
 		super(app);
+		this.parentComponent = parentComponent;
+		this.vault = this.app.vault;
 	}
 
-	onOpen() {
+	async onOpen() {
+		const cardsDB = new CardsDB(this.vault);
+		await cardsDB.load();
+		const allMDs = this.vault.getMarkdownFiles();
+		this.learningSession = new LearningSession(cardsDB, allMDs);
+
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		this.titleEl.innerHTML += "<h4>Simply spaced</h4>";
+		this.root = this.contentEl.createDiv();
+
+		this.doNext();
 	}
 
-	onClose() {
+	private doNext(): void {
+		while (this.root.hasChildNodes()) {
+			this.root.removeChild(this.root.firstChild!);
+		}
+		if (this.learningSession.hasMoreCards()) {
+			this.doNextCard();
+		} else {
+			this.done();
+		}
+	}
+
+	private async doNextCard(): Promise<void> {
+		const cardsCountDiv = document.createElement("div");
+		if (this.learningSession.getLateCount() == 0) {
+			cardsCountDiv.setText(`Today: ${this.learningSession.getTodaysCount()}; New: ${this.learningSession.getNewCount()}`);
+		} else {
+			cardsCountDiv.setText(`Today: ${this.learningSession.getTodaysCount() + this.learningSession.getLateCount()} (Late: ${this.learningSession.getLateCount()}); New: ${this.learningSession.getNewCount()}`);
+		}
+		const questionDiv = document.createElement("div");
+		const b = document.createElement("b");
+		questionDiv.appendChild(b);
+		b.setText(this.learningSession.currentCard().basename);
+		const questionControlDiv = document.createElement("div");
+		const answerDiv = document.createElement("div");
+		const answerControlDiv = document.createElement("div");
+		
+		this.root.appendChild(cardsCountDiv);
+		this.root.appendChild(document.createElement("hr"));
+		this.root.append(questionDiv);
+		this.root.append(answerDiv);
+		this.root.appendChild(document.createElement("hr"));
+		this.root.append(questionControlDiv);
+
+		const showAnswerBtn = document.createElement("button");
+		showAnswerBtn.setText("Show answer");
+		showAnswerBtn.addEventListener("click", async () => {
+			this.root.removeChild(questionControlDiv);
+			this.root.append(answerDiv);
+			this.root.append(answerControlDiv);
+			MarkdownRenderer.renderMarkdown(
+				await this.vault.read(this.learningSession.currentCard()),
+				answerDiv,
+				'',
+				this.parentComponent);
+		});
+		questionControlDiv.appendChild(showAnswerBtn);
+
+
+		const ignoreBtn = document.createElement("button");
+		ignoreBtn.setText("Ignore");
+		ignoreBtn.addEventListener("click", () => {
+			this.learningSession.ignoreCard();
+			this.doNext();
+		});
+		answerControlDiv.appendChild(ignoreBtn);
+		const againBtn = document.createElement("button");
+		againBtn.setText("Again");
+		againBtn.addEventListener("click", () => {
+			this.learningSession.again();
+			this.doNext();
+		});
+		answerControlDiv.appendChild(againBtn);
+		this.createAnswerButton(answerControlDiv, Answer.BLACK_OUT, 'No clue');
+		this.createAnswerButton(answerControlDiv, Answer.WRONG, 'Wrong');
+		this.createAnswerButton(answerControlDiv, Answer.WRONG_EASY_RECALL, 'Wrong, but easy recall');
+		this.createAnswerButton(answerControlDiv, Answer.CORRECT_LOTS_OF_THINKING, 'Right, lots of thinking');
+		this.createAnswerButton(answerControlDiv, Answer.GOOD_SOME_THINKING, 'Right, some hesitation');
+		this.createAnswerButton(answerControlDiv, Answer.PERFECT, 'Perfect');
+
+	}
+
+	private createAnswerButton(container: HTMLDivElement, answer: Answer, text: string): void {
+		const nextIter = getNextInterval(
+			answer,
+			this.learningSession.currentRepetitions(),
+			this.learningSession.currentEasyFactor(),
+			this.learningSession.currentInterval());
+		const btn = document.createElement("button");
+		btn.setText(`${text}(${nextIter.intervalInDays})`);
+		btn.addEventListener("click", () => {
+			this.learningSession.updateCard(
+				nextIter.easyFactor,
+				nextIter.intervalInDays,
+				nextIter.repetiotions,
+				nextIter.shceduledAt
+			);
+			this.doNext();
+		});
+		container.appendChild(btn);
+	}
+
+	private done(): void {
+		this.root.setText('All done');
+	}
+	async onClose() {
 		const {contentEl} = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
